4cd39813678c774971dfe20f7e0074691651d0af
Chadwick Smith <chadwicksmith@Chadwicks-MBP.hsd1.ct.comcast.net>
2021-03-11 11:49:36 -0500

    created calculator, having a hard time getting it to function

---
+++ script.js
@@ -0,0 +1,19 @@
+var firstnum = document.getElementById("num-one").nodeValue;
+var secondnum = document.getElementById("num-two").nodeValue;
+var addvalues = document.getElementById("+").nodeValue;
+    function calcSum(){
+        let sum = Number(firstnum) + Number(secondnum).nodeValue;
+        let addvalues = sum
+        document.getElementById("result").innerText = addvalues
+
+
+     
+     
+ }
+
+     
+  
+    
+
+
+
